8047f414679cffdcf7f6ccade1dc99ea7401509a
Elena Egorova <lelya2402@rambler.ru>
2023-04-25 12:12:41 +0300

    sait

--- sait/scripts/index.js
+++ sait/scripts/index.js
@@ -1,1 +1,21 @@
-const modal=$.modal()
+const modal=$.modal({
+    title: 'Vlad Modal',
+    closable:true,
+    content: `
+        <h4> Modal is</h4>
+        <p>Lorem ipsum</p>
+    `
+    
+    footerButtons: [
+        {
+            text:'OK', type:'primary', handler() {
+                console.log('Primary')
+            }
+        }
+        {
+            text:'Cancel', type:'danger', handler() {
+                console.log('Danger')
+            }
+        }
+    ]
+})
--- sait/scripts/plugins/modal.js
+++ sait/scripts/plugins/modal.js
@@ -2,20 +2,22 @@ function _createModal(options){
     const modal=document.createElement('div')
     modal.classList.add('vmodal')
     modal.insertAdjacentHTML('afterbegin', `    
-        <div class="modal-overlay">
+        <div class="modal-overlay" data-close="true">
             <div class="modal-window">
-                <div class="modal-header">                    
-                    <span class="modal-close">&times</span> <!-- Знак + по которому будем закрывать окно-->
+                <div class="modal-header">
+                    <span class="modal-close" data-close="true">&times</span>               
+                     <!-- Знак + по которому будем закрывать окно-->
                 </div>
 
                 <div class="modal-text">
                     <div class="modal-header-title">
-                        <span class="modal-title">Консультация</span>
+                        <span class="modal-title">${options.title || 'Окно'}Консультация</span>
                         <br><br>
                         <p>Просто оставьте свои контакты и наш менеджер Вас проконсультирует</p>
                     </div>
 
-                    <div class="modal-boby">
+                    <div class="modal-boby" data-content>
+                    ${options.content || ''}
                         <form class="modal-form">
                             <div class="modal-form-name">
                                 <span class="modal-form-text">Ваше имя</span>
@@ -43,19 +45,43 @@ $.modal=function(options){
     const ANIMATION_SPEED=200
     const $modal=_createModal(options)
     let closing=false
-    return {
-        open() {
-            !closing && $modal.classList.add('open')
+    let destroyed= false
+
+const modal = {
+    open() {
+        if (destroyed) {
+            return console.log('Modal is destroyed')
+        }
+        !closing && $modal.classList.add('open')
+    },
+    close() {
+        closing=true
+        $modal.classList.remove('open')
+        $modal.classList.add('hide')
+        setTimeout(() => {
+            $modal.classList.remove('hide')
+            closing=false
+        }, ANIMATION_SPEED)
+    }, 
+}
+
+const listener=event => {
+    console.log('Clicked', event.target.dataset.close)
+        if (event.target.dataset.close) {
+            modal.close()
+        }
+}
+
+    $modal.addEventListener('click', listener)
+
+    return Object.assign(modal, {
+        destroy(){
+            $modal.parentNode.removeChild($modal)
+            $modal.removeEventListener('click',listener)
+            destroyed=true
         },
-        close() {
-            closing=true
-            $modal.classList.remove('open')
-            $modal.classList.add('hide')
-            setTimeout(() => {
-                $modal.classList.remove('hide')
-                closing=false
-            }, ANIMATION_SPEED)
-        }, 
-        destrov() {}        
-    }
+        setContent(html) {
+            $modal.querySelector('[data-content]').innerHTML=html
+        }
+    })
 }
