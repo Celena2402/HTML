27198a79bfb0f93454d91afe85404df56fcf8e16
Elena Egorova <lelya2402@rambler.ru>
2023-05-18 18:01:01 +0300

    sait

--- sait/scripts/script-modal.js
+++ sait/scripts/script-modal.js
@@ -1,30 +1,3 @@
-//$(function () {
-//    $('#callback-button').click(function () {
-//      $('.modal').addClass('modal_active');
-      // $('body').addClass('hidden');
-//    });
-   
-//    $('.modal__close-button').click(function () {
-//      $('.modal').removeClass('modal_active');
-     // $('body').addClass('hidden');
-//    });
-
-//    $('.modal-button').click(function () {
-//        $('.modal').removeClass('modal_active');
-       // $('body').addClass('hidden');
-//      });
-//  });
-
-  // Закрытие модального окна при клике вне его контентной области
- //$('.modal').mouseup(function (e) {
- //   let modalContent = $(".modal__content");
-//   if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
-//      $(this).removeClass('modal_active');
-      // $('body').addClass('hidden');
-//    }
-//  });
-
-
 document.addEventListener('DOMContentLoaded', () => {  
  
     // Кнопка по которой происходит клик
@@ -37,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let closeButton = modal1.getElementsByClassName('modal__close-button')[0];
    
     // Кнопка "закрыть и отправить" внизу модального окна
-    let closeButtonFooter = modal1.getElementsByClassName('modal-button')[0];
+   // let closeButtonFooter = modal1.getElementsByClassName('modal-button')[0];
     // Тег body для запрета прокрутки
     //let tagBody = document.getElementsByTagName('body');
    
@@ -53,11 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
       //tagBody.classList.remove('hidden');
     }
 
-    closeButtonFooter.onclick = function (e) {
-        e.preventDefault();
-        modal1.classList.remove('modal_active');
+    //closeButtonFooter.onclick = function (e) {
+    //    e.preventDefault();
+    //    modal1.classList.remove('modal_active');
         //tagBody.classList.remove('hidden');
-      }
+    //  }
    
     modal1.onmousedown = function (e) {
       let target = e.target;
@@ -79,18 +52,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-
-    // получим кнопку id="btn" с помощью которой будем открывать модальное окно
-    const btn = document.querySelector('#callback-button');
-    // активируем контент id="modal" как модальное окно
-    const modal = new bootstrap.Modal(document.querySelector('#modal-1'));
-    // при нажатии на кнопку
-    btn.addEventListener('callback-button', function() {
-      // открываем модальное окно
-      modal.show();
-    });
   
   });
